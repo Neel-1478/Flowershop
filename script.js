51308339a6e318e58fdf545e648092b009417488
Neel Patel <neelp1478@gmail.com>
2024-04-03 14:08:04 +0530

    hello

--- script.js
+++ script.js
@@ -10,41 +10,17 @@ function calcSal(){
     
   }
 
-  document.addEventListener('DOMContentLoaded', function() {
-    const form = document.querySelector('form');
-    const question1 = form.querySelectorAll('[name="question1"]');
-    const question2 = form.querySelectorAll('[name="question2"]');
-  
-    form.addEventListener('submit', function(event) {
-      event.preventDefault();
-      let score = 0;
-  
-  
-      for (let i = 0; i < question1.length; i++) {
-        if (question1[i].value === 'B') {
-          score++;
-        }
-        else{
-          score=0;
-        }
-      }
-  
-      for (let i = 0; i < question2.length; i++) {
-        if (question2[i].value === 'A') {
-          score++;
-        }
-        else{
-          score=0;
-        }
-      }
+  function calcMarks(){
 
-      
-  
-      alert(`Your score is ${score}`);
-    });
-  });  
-
-
+    let marks=0
+    if(document.querySelector('input[name="question1"]:checked').value=="true"){
+      marks+=2
+    }
+    if(document.querySelector('input[name="question2"]:checked').value=="true"){
+      marks+=2
+    }
+    alert("Your marks is : "+marks + " out of 4")
+}
   function popW(){
     window.open("pop.html","","height=400px width=1400px")
 }  
